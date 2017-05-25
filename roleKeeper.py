7f47e925631775d7c7b925a8c161efe680c9bba6
Vexs <mxgifford@gmail.com>
2017-05-25 17:56:04 -0500

    Add files via upload

--- roleKeeper.py
+++ roleKeeper.py
@@ -15,18 +15,20 @@ class roleKeeper:
 
 
     async def on_member_remove(self, member):
-        if member.nick is not None:
-            self.json_data['member_roles'][member.id] = {
+        if member.nick is None and len(member.roles == 0):
+            return
+        elif member.nick is not None:
+            self.json_data['members'][member.id] = {
                 'roles': [x.id for x in member.roles if x.id in self.saved_roles], 'nickname': member.nick}
         else:
-            self.json_data['member_roles'][member.id] = {
+            self.json_data['members'][member.id] = {
                 'roles': [x.id for x in member.roles if x.id in self.saved_roles], 'nickname': 'None'}
         with open('roles.json','w') as json_file:
             json_file.write(json.dumps(self.json_data, indent=2))
 
     async def on_member_join(self, member):
         try:
-            member_json = self.json_data['member_roles'].pop(member.id)
+            member_json = self.json_data['members'].pop(member.id)
 
             role_list = [discord.Object(id=r_id) for r_id in member_json['roles']]
             await self.bot.add_roles(member, *role_list)
