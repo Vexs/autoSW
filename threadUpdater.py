e6df4bd7d0a2a51282ac4ea07ba05e14d44edd34
Vexs <mxgifford@gmail.com>
2017-06-30 19:03:16 -0500

    Fixed some bugs, cleaned a little.

---
+++ threadUpdater.py
@@ -0,0 +1,78 @@
+import discord
+import asyncio
+import basc_py4chan
+from utils import chanUtils
+import re
+
+
+def get_thread(board, thread_identifier):
+    board = basc_py4chan.Board(board)
+    return next(x for x in board.get_all_threads() if thread_identifier in x.topic.subject)
+
+
+async def last_post_id_get(bot, channel):
+    regex = re.compile(r'No\. (\d+)')
+    async for message in bot.logs_from(channel, limit=100):
+        if message.author.id == bot.user.id:
+            if message.embeds:
+                try:
+                    search = re.search(regex, message.embeds[0]['title'])
+                    if search is None:
+                        pass
+                    if search.group(1) == '':
+                        pass
+                    return int(search.group(1))
+                except Exception:
+                    pass
+    return None
+
+
+async def update_thread(bot, board_name, thread_name, general_channel_id, mirror_channel_id):
+    await bot.wait_until_ready()
+
+    general_channel = discord.Object(id=general_channel_id)
+    mirror_channel = discord.Object(id=mirror_channel_id)
+
+    last_post_id = await last_post_id_get(bot, mirror_channel)
+
+    thread = await bot.loop.run_in_executor(None, get_thread, board_name, thread_name)
+    await bot.loop.run_in_executor(None, thread.update)
+
+    if not any(x for x in thread.posts if x.post_id == last_post_id):
+        last_post_id = None
+
+    while not bot.is_closed:
+        if last_post_id is not None:
+            new_posts = sum(1 for x in thread.all_posts if x.post_id > last_post_id)
+            last_post_id = None
+        else:
+            new_posts = await bot.loop.run_in_executor(None, thread.update)
+
+        if new_posts == 0:
+            new_thread = await bot.loop.run_in_executor(None, get_thread, board_name, thread_name)
+
+            if thread.url != new_thread.url and thread.topic.datetime < new_thread.topic.datetime:
+                thread = new_thread
+                await bot.send_message(general_channel, "New thread at: " + thread.url)
+
+        for post in thread.posts[len(thread.posts) - new_posts:]:
+            await bot.send_message(mirror_channel, embed=chanUtils.posttoembed(post))
+
+        await asyncio.sleep(60)
+
+
+class ThreadUpdater:
+    """"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.loop = self.bot.loop
+        self.thread_update_task = self.bot.loop.create_task(update_thread(self.bot, 'vg', 'Titanfall General',
+                                                                          '298667118810103808', '275869654919151617'))
+
+    def __unload(self):
+        self.thread_update_task.cancel()
+
+
+def setup(bot):
+    bot.add_cog(ThreadUpdater(bot))
